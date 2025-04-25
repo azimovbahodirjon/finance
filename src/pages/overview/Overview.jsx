b8fd6a446885377db6c65041b98b950247fa1e2c
Bahodirjon <Bahodirjon@gmail.com>
2025-04-25 17:40:05 +0300

    refactor Overview.scss to improve layout, add loader animation, and enhance button styles

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -3,10 +3,11 @@ import "./Overview.scss";
 
 function Overview() {
   const { signout, isPending } = useLogout();
+
   return (
     <div className="overview">
       <button className="logout-btn" onClick={() => signout()}>
-        {isPending ? "Loading..." : "Logout"}
+        {isPending ? <span className="loader"></span> : "Logout"}
       </button>
     </div>
   );
